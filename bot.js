9e1c5c9c576afc4b045c46328bb66c675d443547
animsaj <animsaj70@gmail.com>
2017-03-09 17:29:09 +0200

    private and public search added

--- bot.js
+++ bot.js
@@ -13,32 +13,72 @@ module.exports = botBuilder(function (request) {
 			topic,
 			url,
 			reqArr = request.text.split(" ");
-		if(reqArr.length > 1) {
+			var options = {
+				uri: url,
+				headers: {
+					'User-Agent': 'Request-Promise'
+				},
+				json: true // Automatically parses the JSON string in the response 
+			};
+		if(reqArr.length > 2 && reqArr[0] === "show") {
+			topic = reqArr.splice(reqArr.length - 1)[0];
+			q = reqArr.slice(1).join(" ");
+			url = "https://developer.mozilla.org/en-US/search.json?q=" + q + "?topic=" + topic;
+			return rp(options)
+			.then(function (data) {
+				var publicResultWithTopic = new slackTemplate("The results of search for: " + request.text);
+				return publicResultWithTopic
+				.addAttachment('A1')
+				.addTitle(data.documents[0].title, data.documents[0].url)
+				.addText(data.documents[0].excerpt.replace(/(<([^>]+)>)/ig,"")).get();
+			})
+			.catch(function (err) {
+				console.log(err); 
+			});
+		} else if(reqArr.length > 1 && reqArr[0] === "show") {
+			q = reqArr.slice(1).join(" ");
+			url = "https://developer.mozilla.org/en-US/search.json?q=" + q;
+			return rp(options)
+			.then(function (data) {
+				var publicResult = new slackTemplate("The results of search for: " + request.text);
+				return publicResult
+				.addAttachment('A1')
+				.addTitle(data.documents[0].title, data.documents[0].url)
+				.addText(data.documents[0].excerpt.replace(/(<([^>]+)>)/ig,"")).get();
+			})
+			.catch(function (err) {
+				console.log(err); 
+			});
+		} else if(reqArr.length > 1) {
 			topic = reqArr.splice(reqArr.length - 1)[0];
 			q = reqArr.join(" ");
 			url = "https://developer.mozilla.org/en-US/search.json?q=" + q + "?topic=" + topic;
+			return rp(options)
+			.then(function (data) {
+				var privatResultWithTopic = new slackTemplate("The results of search for: " + request.text);
+				data.documents.forEach(function (entry) {
+					return privatResultWithTopic.addAttachment('A1').addTitle(entry.title, entry.url).addText(entry.excerpt.replace(/(<([^>]+)>)/ig,""));
+				});
+				return privatResultWithTopic.get();
+			})
+			.catch(function (err) {
+				console.log(err); 
+			});
 		} else {
 			q = reqArr.join(" ");
 			url = "https://developer.mozilla.org/en-US/search.json?q=" + q;
-		}
-		var options = {
-    		uri: url,
-			headers: {
-				'User-Agent': 'Request-Promise'
-			},
-			json: true // Automatically parses the JSON string in the response 
-		};
-		return rp(options)
-		.then(function (data) {
-			var searchResults = new slackTemplate("The results of search for: " + request.text);
-			data.documents.forEach(function (entry) {
-				return searchResults.addAttachment('A1').addTitle(entry.title, entry.url).addText(entry.excerpt);
+			return rp(options)
+			.then(function (data) {
+				var privateResult = new slackTemplate("The results of search for: " + request.text);
+				data.documents.forEach(function (entry) {
+					return privateResult.addAttachment('A1').addTitle(entry.title, entry.url).addText(entry.excerpt.replace(/(<([^>]+)>)/ig,""));
+				});
+				return privateResult.get();
+			})
+			.catch(function (err) {
+				console.log(err); 
 			});
-			return searchResults.channelMessage(true).get();
-		})
-		.catch(function (err) {
-			console.log(err); 
-		});
+		}
 	}
 });
 
